395cf102c99a302a973c6fc06b507ea239312407
Termux Builder <termux@example.com>
2026-08-24 17:14:33 +0600

    Build request 1787570073

---
+++ src/com/example/testapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello from ZIP project build!");
+        setContentView(tv);
+    }
+}
